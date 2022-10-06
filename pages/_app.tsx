ce6f3da6225e9e5f411857604a52dfc627e4eec5
Xabier Lameiro Cardama <xabier.lameirocardama@plexus.es>
2022-10-06 13:09:01 +0200

    Change script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,20 +6,16 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
   <>
               <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-              <Script
-                id="google-analytics"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                  __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                  page_path: window.location.pathname,
-                  });
-              `,
-                }}
-              />
+              <Script id="ga-script" strategy="afterInteractive">
+                  {`
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                      page_path: window.location.pathname,
+                      });
+                  `}
+              </Script>
               <Component {...pageProps} />
 
         </>     
