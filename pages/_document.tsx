ebc808d93a2bb4e7d794728860c06a4e8c40997a
Xabier Lameiro Cardama <xabier.lameirocardama@plexus.es>
2022-10-07 07:38:29 +0200

    Fix error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,12 +20,12 @@ class MyDocument extends Document {
 				<link rel="icon" href="/favicon.svg" />
 				<meta
 					name="description"
-					content="Xabi and Maria and we are looking for accommodation in Limerick city, if you are interested please contact us."
+					content="Xabi and Maria we are looking for accommodation in Limerick city, if you are interested please contact us."
 					/>
 					<meta property="og:site_name" content="couplelookinghomeinlimerick"></meta>
 					<meta property="og:type" content="article" />
 					<meta property="og:title" content="We are a couple looking for housing in Limerick city, I am a programmer and my girlfriend is a teacher. We are serious, friendly and do not smoke." />
-					<meta property="og:description" content="Xabi and Maria and we are looking for accommodation in Limerick city, if you are interested please contact us."/>
+					<meta property="og:description" content="Xabi and Maria we are looking for accommodation in Limerick city, if you are interested please contact us."/>
 					<meta property="og:image" content="https://couplelookinghomeinlimerick.com/xabiymaria.jpg" />
 					<meta property="og:url" content=" http://www.example.com/" />
 					<meta name="robots" content="all" />
