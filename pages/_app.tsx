09934e697ae89779529d74576604ddc135aa9e8e
Xabier Lameiro Cardama <xabier.lameirocardama@plexus.es>
2022-10-07 19:48:29 +0200

    Changes on script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
   <>
               <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-              <Script strategy="afterInteractive" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3537017956623483" crossOrigin="anonymous"/>
               <Script id="ga-script" strategy="afterInteractive">
                   {`
                       window.dataLayer = window.dataLayer || [];
@@ -18,6 +17,11 @@ function MyApp({ Component, pageProps }: AppProps) {
                       gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ADDS}');
                   `}
               </Script>
+              <Script id="Adsense-id" data-ad-client="ca-pub-3537017956623483"
+                  async strategy="afterInteractive"
+                  onError={ (e) => { console.error('Script failed to load', e) }}
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+                />
               <Component {...pageProps} />
 
         </>     
