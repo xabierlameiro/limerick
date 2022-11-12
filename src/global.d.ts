e16a979353cb6f15ab56b08fac30587c5198b9e2
Xabier Lameiro Cardama <xabier.lameirocardama@plexus.es>
2022-11-12 19:51:40 +0000

    Minor changes

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,3 +2,20 @@ declare module "*.mdx" {
     const content: string;
     export default content;
 }
+
+declare namespace google.maps {
+    class Size {
+        width: number;
+        height: number;
+        equals?(other: Size): boolean;
+    }
+    class DirectionsService {
+        route(
+            request: DirectionsRequest,
+            callback: (
+                result: DirectionsResult,
+                status: DirectionsStatus
+            ) => void
+        ): Promise;
+    }
+}
