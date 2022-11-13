29b65e2e430244fc29901620c8a2714ad413a3f0
Xabier Lameiro Cardama <xabier.lameirocardama@plexus.es>
2022-11-13 15:17:11 +0000

    Remove file

--- src/configs/firebase-messaging-sw.ts
+++
@@ -1,7 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging/sw";
-import { getFirebaseConfig } from "@/configs/firebase-config";
-
-const firebaseApp = initializeApp(getFirebaseConfig());
-getMessaging(firebaseApp);
-console.info("Firebase messaging service worker is set up");
