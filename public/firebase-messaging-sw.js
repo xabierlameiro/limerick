dfe0309d3d6abafd125c1903b4039a94748ddfe9
Xabier Lameiro Cardama <xabier.lameirocardama@plexus.es>
2022-11-14 11:51:46 +0000

    Minor change

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -52,28 +52,28 @@ try {
             notification.title,
             notificationOptions
         );
+    });
+    self.addEventListener("notificationclick", (event) => {
+        console.log("On notification click: ", event.notification.tag);
+        event.notification.close();
 
-        self.addEventListener("notificationclick", (event) => {
-            console.log("On notification click: ", event.notification.tag);
-            event.notification.close();
-
-            // This looks to see if the current is already open and
-            // focuses if it is
-            event.waitUntil(
-                clients
-                    .matchAll({
-                        type: "window",
-                    })
-                    .then((clientList) => {
-                        console.log("clientList", clientList);
-                        for (const client of clientList) {
-                            if (client.url === "/" && "focus" in client)
-                                return client.focus();
-                        }
-                        if (clients.openWindow) return clients.openWindow("/");
-                    })
-            );
-        });
+        // This looks to see if the current is already open and
+        // focuses if it is
+        event.waitUntil(
+            clients
+                .matchAll({
+                    type: "window",
+                })
+                .then((clientList) => {
+                    console.log("clientList", clientList);
+                    for (const client of clientList) {
+                        if (client.url === "/search" && "focus" in client)
+                            return client.focus();
+                    }
+                    if (clients.openWindow)
+                        return clients.openWindow("/search");
+                })
+        );
     });
 } catch (e) {
     console.error("firebase-messaging-sw.js error", e);
