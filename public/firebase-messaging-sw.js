e4579ac8d3673e48f63c66ea5678fd88629016a1
Xabier Lameiro Cardama <xabier.lameirocardama@plexus.es>
2022-11-14 11:14:06 +0000

    Test

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -45,12 +45,35 @@ try {
             ...notification,
             icon: notification.image,
             lang: "en-IE",
+            requireInteraction: true,
         };
 
         self.registration.showNotification(
             notification.title,
             notificationOptions
         );
+
+        self.addEventListener("notificationclick", (event) => {
+            console.log("On notification click: ", event.notification.tag);
+            event.notification.close();
+
+            // This looks to see if the current is already open and
+            // focuses if it is
+            event.waitUntil(
+                clients
+                    .matchAll({
+                        type: "window",
+                    })
+                    .then((clientList) => {
+                        console.log("clientList", clientList);
+                        for (const client of clientList) {
+                            if (client.url === "/" && "focus" in client)
+                                return client.focus();
+                        }
+                        if (clients.openWindow) return clients.openWindow("/");
+                    })
+            );
+        });
     });
 } catch (e) {
     console.error("firebase-messaging-sw.js error", e);
